abfb1a41009572c3efdab0d5387f07985b390b8f
Mike Jolley <mike.jolley@me.com>
2015-11-24 14:29:02 +0000

    Updater

--- includes/updater/views/html-key-input.php
+++ includes/updater/views/html-key-input.php
@@ -2,10 +2,10 @@
 	<td class="plugin-update" colspan="3">
 		<?php $this->error_notices(); ?>
 		<div class="wpjm-updater-licence-key">
-			<label for="<?php echo sanitize_title( $this->plugin_slug ); ?>_licence_key"><?php _e( 'Licence' ); ?>:</label>
-			<input type="text" id="<?php echo sanitize_title( $this->plugin_slug ); ?>_licence_key" name="<?php echo esc_attr( $this->plugin_slug ); ?>_licence_key" placeholder="Licence key" />
+			<label for="<?php echo sanitize_title( $this->plugin_slug ); ?>_licence_key"><?php _e( 'License' ); ?>:</label>
+			<input type="text" id="<?php echo sanitize_title( $this->plugin_slug ); ?>_licence_key" name="<?php echo esc_attr( $this->plugin_slug ); ?>_licence_key" placeholder="XXXX-XXXX-XXXX-XXXX" />
 			<input type="email" id="<?php echo sanitize_title( $this->plugin_slug ); ?>_email" name="<?php echo esc_attr( $this->plugin_slug ); ?>_email" placeholder="Email address" value="<?php echo esc_attr( get_option( 'admin_email' ) ); ?>" />
-			<span class="description"><?php _e( 'Enter your licence key and email and hit return. A valid key is required for automatic updates.' ); ?></span>
+			<span class="description"><?php _e( 'Enter your license key and email and hit return. A valid key is required for updates.' ); ?> <?php printf( 'Lost your key? <a href="%s">Retrieve it here</a>.', esc_url( 'https://wpjobmanager.com/lost-licence-key/' ) ); ?></span>
 		</div>
 	</td>
 	<script>
